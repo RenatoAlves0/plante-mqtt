68f38839b1d284394d8373eb8ef1239678acfd19
renatoalves0 <renatoalves97.123@gmail.com>
2021-01-12 12:31:02 -0300

    save X testes em files diferentes automaticamente

--- cliente.js
+++ cliente.js
@@ -2,34 +2,37 @@ var mqtt = require('mqtt')
 var client_mqtt = mqtt.connect({ host: 'localhost', port: 1883 })
 const NTP = require("ntp-time").Client
 const client_ntp = new NTP("a.st1.ntp.br", 123, { timeout: 5000 })
-let delay_anterior = 0
+const jsonexport = require('jsonexport')
+fs = require('fs')
+let dados = { chegada: undefined, delay_ms: undefined, jitter_ms: undefined }
+let delay_anterior = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let array_dados = [[], [], [], [], [], [], [], [], [], []]
 
 client_mqtt.on('connect', function () {
-    client_mqtt.subscribe('sensores', function (err) {
+    client_mqtt.subscribe(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], function (err) {
         // if (!err) {
         //     client_mqtt.publish('presence', 'Hello mqtt')
         // }
     })
 })
 
-client_mqtt.on('message', function (topic, message) {
-    let dados = { chegada: undefined, delay_ms: undefined, jitter_ms: undefined }
+client_mqtt.on('message', async (topic, message) => {
+    console.log('Tópico: ' + topic.toString())
+    console.log('Tamanho Tópico: ' + Buffer.byteLength(topic) + ' bytes')
+    console.log('Tamanho Mensagem: ' + Buffer.byteLength(message) + ' bytes')
     dados = JSON.parse(message.toString())
-    // dados.chegada = Date.now()
     dados.envio = data_saida(dados.envio_s, dados.envio_us)
 
     client_ntp
         .syncTime()
-        .then((time) => {
-            dados.chegada = time.transmitTimestamp.toString().split('.')[0]
-            let envio = new Date(parseInt(dados.envio))
-            let chegada = new Date(parseInt(dados.chegada))
-            dados.delay_ms = Math.abs(chegada - envio)
-
-            dados.jitter_ms = Math.abs(delay_anterior - dados.delay_ms)
-            console.log(dados)
-
-            delay_anterior = dados.delay_ms
+        .then(async (time) => {
+            dados.chegada = await time.transmitTimestamp.toString().split('.')[0]
+            let envio = await new Date(parseInt(dados.envio))
+            let chegada = await new Date(parseInt(dados.chegada))
+            dados.delay_ms = await Math.abs(chegada - envio)
+            dados.jitter_ms = await Math.abs(delay_anterior[parseInt(topic)] - dados.delay_ms)
+            delay_anterior[parseInt(topic)] = await dados.delay_ms
+            await dadosToFile(dados, parseInt(topic))
         })
 
     // client_mqtt.end()
@@ -41,7 +44,30 @@ data_saida = (envio_s, envio_us) => {
     let qtd_zero = 6 - envio_us.length
     let qtd_ms = 3 - qtd_zero
     for (let i = 0; i < qtd_zero; i++)
-        envio_s += "0"
+        envio_s += '0'
     envio_s += envio_us.slice(0, qtd_ms)
     return envio_s
+}
+
+dadosToFile = async (dados, i) => {
+    console.log('i ===> ' + i)
+    console.log('dados.id ===> ' + dados.id)
+    await array_dados[i].push(dados)
+    if (dados.id >= 10) {
+        // if (dados.id == '0') 
+        await jsonexport(array_dados[i], async (err, csv) => {
+            if (err) return console.error(err)
+            let arquivo = await csv
+            await fs.writeFile('c_' + i + '.csv', arquivo, async (err) => {
+                if (err) return console.log(err)
+            })
+        })
+        // else await jsonexport(dados, includeHeaders = false, async (err, csv) => {
+        //     if (err) return console.error(err)
+        //     let arquivo = await csv
+        //     fs.appendFile('c_' + i + '.csv', arquivo, async (err) => {
+        //         if (err) return console.log(err)
+        //     })
+        // })
+    }
 }
